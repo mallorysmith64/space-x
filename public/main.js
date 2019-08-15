1d9109cee90a0d630b958c0988ddf06055051237
Mallory Smith <mallorysmith6464@gmail.com>
2019-08-15 14:39:56 -0400

    started working on api

--- public/main.js
+++ public/main.js
@@ -1,7 +1,24 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+const SPACE_X = () => {
+  const input = document.querySelector('input').value
+  const url = 'https://api.openweathermap.org/data/2.5/'
+  const key = '2823a9a190fd597e6803d812db33835c'
+  const location = `${url}weather?q=${input}&appid=${key}`
+  fetch(location)
+    .then(response => {
+      console.log(response)
+      return response.json()
+    })
+    .then(city => {
+      console.log(city)
+      document.querySelector(".").textContent = 
+      document.querySelector('.').textContent = 
+      document.querySelector('.').textContent = 
+      document.querySelector('.').textContent = 
+    })
 }
+//todo: add geolocation
+navigator.geolocation.getCurrentPosition(function(position) {
+  console.log(position.coords.latitude, position.coords.longitude)
+})
 
-document.addEventListener('DOMContentLoaded', main)
+document.querySelector('button').addEventListener('click', getWeather)
