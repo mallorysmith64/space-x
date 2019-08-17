0a50f273fd11f60e4ac27152f532e845fd7c0105
Mallory Smith <mallorysmith6464@gmail.com>
2019-08-17 13:38:33 -0400

    added background image

--- public/main.js
+++ public/main.js
@@ -1,24 +1,21 @@
-const SPACE_X = () => {
-  const input = document.querySelector('input').value
-  const url = 'https://api.openweathermap.org/data/2.5/'
-  const key = '2823a9a190fd597e6803d812db33835c'
-  const location = `${url}weather?q=${input}&appid=${key}`
-  fetch(location)
-    .then(response => {
-      console.log(response)
-      return response.json()
-    })
-    .then(city => {
-      console.log(city)
-      document.querySelector(".").textContent = 
-      document.querySelector('.').textContent = 
-      document.querySelector('.').textContent = 
-      document.querySelector('.').textContent = 
-    })
-}
-//todo: add geolocation
-navigator.geolocation.getCurrentPosition(function(position) {
-  console.log(position.coords.latitude, position.coords.longitude)
-})
+//make space-x background image
 
-document.querySelector('button').addEventListener('click', getWeather)
+const makeUrl = spaceX => {
+  const API_URL =
+    'https://sdg-astro-api.herokuapp.com/api/SpaceX/launches/upcoming'
+
+  fetch(API_URL, {
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8'
+    }
+  }).then(response => {
+    console.log()
+    return response.json()
+  })
+}
+
+const main = () => {
+  makeUrl()
+}
+
+document.addEventListener('DOMContentLoaded', main)
