caebeab576ab9994ae0a001a87b07537e2772d72
Mallory Smith <mallorysmith6464@gmail.com>
2019-08-18 18:20:22 -0400

    broke then fixed countdown timer

--- public/main.js
+++ public/main.js
@@ -55,49 +55,50 @@ GenerateMission = launch => {
   let launchLocation = document.createElement('li')
   launchLocation.textContent = launch.launch_site.site_name_long
   parent.appendChild(launchLocation)
-}
 
-//make countdown timer
-const timer = setInterval(function() {
-  //set current date
-  let today = new Date()
-  //set end date or launch date
-  let timeLaunch = new Date(launch.launch_date_utc)
-  //find time between start and end dates
-  let findDate = timeLaunch.getTime() - today.getTime()
-  //change to total seconds
-  let timeDifference = findDate / 1e3
-  //covert to days, hours, minutes,seconds
-  let totalSeconds = Math.abs(timeDifference)
-  if (timeDifference < 0) {
-    clearInterval(timer)
-    document.querySelector('.countdown').textContent = 'Launch!'
-  } else {
-    const time = {
-      days: 0,
-      hours: 0,
-      minutes: 0,
-      seconds: 0
+  //make countdown timer
+  const timer = setInterval(function() {
+    //set current date
+    let today = new Date()
+    //set end date or launch date
+    let timeLaunch = new Date(launch.launch_date_utc)
+    //find time between start and end dates
+    let findDate = timeLaunch.getTime() - today.getTime()
+    //change to total seconds
+    let timeDifference = findDate / 1e3
+    //covert to days, hours, minutes,seconds
+    let totalSeconds = Math.abs(timeDifference)
+
+    if (timeDifference < 0) {
+      clearInterval(timer)
+      document.querySelector('.countdown').textContent = 'Launch!'
+    } else {
+      const time = {
+        days: 0,
+        hours: 0,
+        minutes: 0,
+        seconds: 0
+      }
+      // convert time seconds to be displayed on card
+      time.days = Math.floor(totalSeconds / (60 * 60 * 24))
+      totalSeconds = totalSeconds - time.days * 24 * 60 * 60
+      time.hours = Math.floor(totalSeconds / (60 * 60))
+      totalSeconds = totalSeconds - time.hours * 60 * 60
+      time.minutes = Math.floor(totalSeconds / 60)
+      totalSeconds = totalSeconds - time.minutes * 60
+      time.seconds = Math.floor(totalSeconds)
+      document.querySelector('.countdown').textContent =
+        time.days +
+        ' days, ' +
+        time.hours +
+        ' hours, ' +
+        time.minutes +
+        ' minutes, ' +
+        time.seconds +
+        ' seconds'
     }
-    // convert time seconds to be displayed on card
-    time.days = Math.floor(totalSeconds / (60 * 60 * 24))
-    totalSeconds = totalSeconds - time.days * 24 * 60 * 60
-    time.hours = Math.floor(totalSeconds / (60 * 60))
-    totalSeconds = totalSeconds - time.hours * 60 * 60
-    time.minutes = Math.floor(totalSeconds / 60)
-    totalSeconds = totalSeconds - time.minutes * 60
-    time.seconds = Math.floor(totalSeconds)
-    document.querySelector('.countdown').textContent =
-      time.days +
-      ' days, ' +
-      time.hours +
-      ' hours, ' +
-      time.minutes +
-      ' minutes, ' +
-      time.seconds +
-      ' seconds'
-  }
-}, 4000)
+  }, 2000)
+}
 
 const main = () => {
   makeUrl()
